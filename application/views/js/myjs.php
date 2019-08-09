776a88f62842c4ef419c1b7182d3e16b616e3487
assanai_laptop <assanai_yub1807@hotmail.co.th>
2019-08-09 14:04:53 +0700

    transfer to pc

--- application/views/js/myjs.php
+++ application/views/js/myjs.php
@@ -1,20 +1,14 @@
-<!-- <script src="<?php echo base_url('assets/js/bs_js/bootstrap.min.js'); ?>"></script> -->
-<!-- <script src="<?php echo base_url('assets/js/jquery.min.js'); ?>"></script> -->
-<!-- <script src="<?php echo base_url('assets/js/vue.min.js'); ?>"></script> -->
-<!-- <script src="<?php echo base_url('assets/js/script.js'); ?>"></script> -->
-
-<!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" 
-	integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" 
-	crossorigin="anonymous"></script> -->
+<script src="<?php echo base_url('assets/js/jquery.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/popper.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/tooltip.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/bootstrap/js/bootstrap.min.js'); ?>"></script>
 <script src="<?php echo base_url('assets/js/script.js'); ?>"></script>
-<script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
 <script src="https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.js"></script>
-<!-- <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script> -->
-<script src="<?php echo base_url('assets/js/jquery-ui-1.12.1/jquery-ui.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/jquery-ui/jquery-ui.min.js'); ?>"></script>
 <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
+<!-- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script> -->
+<script src="<?php echo base_url('assets/sweetalert2/src/sweetalert2.js'); ?>"></script>
 
 <script type="text/javascript" src="<?php echo base_url('assets/js/bootstrap-datepicker.js'); ?>"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/bootstrap-datepicker-thai.js'); ?>"></script>
