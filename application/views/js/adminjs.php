d6ba1d308ed0a6ab96a0673c2ba9a217079f789a
assanai_pc <assanai_yub1807@hotmail.co.th>
2019-08-26 17:03:04 +0700

    commit on last day 2019-08-26

--- application/views/js/adminjs.php
+++ application/views/js/adminjs.php
@@ -1,8 +1,8 @@
 <script src="<?php echo base_url('assets/admin/vendor/jquery-easing/jquery.easing.min.js'); ?>"></script>
 
-<script src="<?php echo base_url('assets/admin/vendor/chart.js/Chart.min.js'); ?>"></script>
+<!-- <script src="<?php echo base_url('assets/admin/vendor/chart.js/Chart.min.js'); ?>"></script> -->
 
-<script src="<?php echo base_url('assets/admin/js/demo/chart-area-demo.js'); ?>"></script>
-<script src="<?php echo base_url('assets/admin/js/demo/chart-pie-demo.js'); ?>"></script>
+<!-- <script src="<?php echo base_url('assets/admin/js/demo/chart-area-demo.js'); ?>"></script> -->
+<!-- <script src="<?php echo base_url('assets/admin/js/demo/chart-pie-demo.js'); ?>"></script> -->
 
 <!-- <script src="<?php echo base_url('assets/js/admin-script.js'); ?>" type="text/javascript"></script> -->
